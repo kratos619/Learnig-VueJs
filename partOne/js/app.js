100ac787d870de54098ac35a171a062885c3d56e
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-19 19:21:55 +0530

    Biding Links
    {{}} not working in all html elements
    use v-bind:href with vue ja object to add link to html page

--- partOne/js/app.js
+++ partOne/js/app.js
@@ -1,11 +1,15 @@
 new Vue({
   el: "#app",
   data: {
-    title: "hi iam Vue Js!"
+    title: "hi iam Vue Js from title!",
+    link: "www.google.com"
   },
   methods: {
     changeTitle: function(event) {
       this.title = event.target.value;
+    },
+    sayhello: function() {
+      return this.title;
     }
   }
 });
