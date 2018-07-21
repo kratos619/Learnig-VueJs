6f94cf4f712290b3bf3a14f9456112aaddaa3dc5
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-21 19:25:47 +0530

    dynamic Styling With Vue

--- TwoWaybinding/js/app.js
+++ TwoWaybinding/js/app.js
@@ -1,13 +1,3 @@
 new Vue({
-  el: "#app",
-  data: {
-    counter: 0,
-    result: ""
-  },
-  methods: {
-    increse: function() {
-      this.counter++;
-      this.result = this.counter > 5 ? "Greater 5" : "smaller 5";
-    }
-  }
+  el: "#app"
 });
