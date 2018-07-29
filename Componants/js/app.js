3915ee0c6a4f4c01a6d5450441448e9cbfa35e18
Gaurav Palaspagar - PC <gaurav.palaspagar@hotmail.com>
2018-07-29 15:52:52 +0530

    CreateOwn Componant

--- Componants/js/app.js
+++ Componants/js/app.js
@@ -1,31 +1,14 @@
+Vue.component('app-cmp', {
+  data: function () {
+    return {
+      status: 'critical'
+    }
+
+  },
+  template: '<p> server Status: {{status}}</p>'
+});
+
 new Vue({
   el: "#app",
-  data: {
-    title: "hi iam Vue Js from title!",
-    link: "www.google.com",
-    // link output by rawhtml
-    fulllink: '<a href="www.google.com">go to google </a>',
-    counter: 0,
-    x: 0,
-    y: 0
-  },
-  methods: {
-    changeTitle: function(event) {
-      this.title = event.target.value;
-    },
-    sayhello: function() {
-      this.title = "world";
-      return this.title;
-    },
-    incressCounter: function(step) {
-      this.counter += step;
-    },
-    updateCorr: function(event) {
-      this.x = event.clientX;
-      this.y = event.clientY;
-    },
-    alertme: function() {
-      alert("you enter space");
-    }
-  }
+
 });
